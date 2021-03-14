b84cdb6e7df2502ce9a37d8e04207fdab0db0b31
Rex Mitchell <nogardjmj@gmail.com>
2021-03-14 14:07:45 -0500

    Testing sync

--- app/GHIT/settings.py
+++ app/GHIT/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    # 'issues.apps.IssuesConfig',
+    # 'projects.apps.ProjectsConfig',
+    # 'sync.apps.SyncConfig',
+    # 'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- app/sync/views.py
+++ app/sync/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'sync/testgh.html', {'user': user})
