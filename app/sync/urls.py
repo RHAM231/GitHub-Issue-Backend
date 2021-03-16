2ce23e83a7ea341df305dbcddc34f485bcaeca16
Rex Mitchell <nogardjmj@gmail.com>
2021-03-15 19:17:25 -0500

    Exploring pygithub functionality

--- app/sync/urls.py
+++ app/sync/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('confirm-sync', views.confirm_sync, name='confirm-sync'),
     # path('sync-success', views.sync_success, name='sync-success'),
-    path('github', views.github, name='test'),
+    path('github/', views.github, name='test'),
+    path('github-client/', views.github_client, name='test2'),
 ]
--- app/sync/views.py
+++ app/sync/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 import requests
+from django.conf import settings
+import os
+from github import Github, GithubException
 
 
 def confirm_sync(request):
@@ -22,6 +25,10 @@ def sync_success(request):
 
 def github(request):
     search_result = {}
+
+    print('PRINTING TEST')
+    test = settings.TEST
+    print(test)
     if 'username' in request.GET:
         username = request.GET['username']
         url = 'https://api.github.com/users/%s' % username
@@ -33,4 +40,30 @@ def github(request):
             'limit': response.headers['X-RateLimit-Limit'],
             'remaining': response.headers['X-RateLimit-Remaining'],
         }
+    return render(request, 'sync/github.html', {'search_result': search_result})
+
+
+def github_client(request):
+    search_result = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        # client = Github(settings.TEST_TOKEN)
+        client = Github()
+
+        try:
+            user = client.get_user(username)
+            search_result['name'] = user.name
+            search_result['login'] = user.login
+            search_result['public_repos'] = user.public_repos
+            search_result['success'] = True
+        except GithubException as ge:
+            search_result['message'] = ge.data['message']
+            search_result['success'] = False
+
+        # rate_limit = client.get_rate_limit()
+        search_result['rate'] = {
+            # 'limit': rate_limit.rate.limit,
+            # 'remaining': rate_limit.rate.remaining,
+        }
+
     return render(request, 'sync/github.html', {'search_result': search_result})
