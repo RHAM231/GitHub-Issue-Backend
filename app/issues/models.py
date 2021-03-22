cfbe4de66817888a714fc3311b877ed195a26bd0
Rex Mitchell <nogardjmj@gmail.com>
2021-03-21 21:56:52 -0500

    Building database structure

--- app/issues/models.py
+++ app/issues/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 class Issue(models.Model):
     name = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    body = models.CharField(max_length=100)
+    # created_at = 
+    # updated_at = 
+    # closded_at = 
+    # number = 
     
 
     def __str__(self):
--- app/repositories/models.py
+++ app/repositories/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    open_issues_count = models.CharField(max_length=100)
+    # updated_at = 
+
+    def __str__(self):
+        return self.name
+
+
+class RepoFolder(models.Model):
+    name = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+    sha = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    data_type = models.CharField(max_length=100)
+    mode = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class RepoFile(models.Model):
+    name = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+    data_type = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    encoding = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    sha = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
