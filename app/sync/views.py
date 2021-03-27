8e07c4d945178005b58c0875780b723eb38e0fc9
Rex Mitchell <nogardjmj@gmail.com>
2021-03-27 15:11:38 -0500

    Building client logic to interface with GitHub

--- app/sync/views.py
+++ app/sync/views.py
@@ -4,34 +4,32 @@ from django.conf import settings
 import os
 from github import Github, GithubException
 from pprint import pprint
+import json
 
 
-def get_query_url(key, *args, **kwargs):
+def get_query_url(lookup, branch=None, sha=None, path=None, issue_id=None):
+    endpoint = "https://api.github.com/repos/RHAM231-IssueTracker/IssueTrackerSandbox"
     query_dict = {
         # Rate Limit
-        rate_limit: "https://api.github.com/rate_limit",
-
+        'rate_limit': "https://api.github.com/rate_limit",
         # Repositories
-        get_repo: f"https://api.github.com/repos/{owner}/{repo}",
-        list_branches: f"https://api.github.com/repos/{owner}/{repo}/branches",
-        get_branch: f"https://api.github.com/repos/{owner}/{repo}/branches/{branch}",
-
+        'get_repo': endpoint,
+        'list_branches': endpoint + '/branches',
+        'get_branch': endpoint + f'/branches/{branch}',
         # Folders
-        get_root_repo_tree: f"https://api.github.com/repos/{owner}/{repo}/git/trees/{sha}",
-        get_folder_tree: f"https://api.github.com/repos/{owner}/{repo}/git/trees/{sha}",
-
+        'get_root_repo_tree': endpoint + f'/git/trees/{sha}',
+        'get_folder_tree': endpoint + f'/git/trees/{sha}',
         # Files
-        get_file_contents: f"https://api.github.com/repos/{owner}/{repo}/contents/{path}",
-
+        'get_file_contents': endpoint + f'/contents/{path}',
         # Issues
-        repo_issues: f"https://api.github.com/repos/{owner}/{repo}/issues",
-        create_issue: f"https://api.github.com/repos/{owner}/{repo}/issues",
-        get_issue: f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}",
-        update_issue: f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}",
-        close_issue: f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}",
-        open_issue: f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}",
+        'repo_issues': endpoint + '/issues',
+        'create_issue': endpoint + '/issues',
+        'get_issue': endpoint + f'/issues/{issue_id}',
+        'update_issue': endpoint + f'/issues/{issue_id}',
+        'close_issue': endpoint + f'/issues/{issue_id}',
+        'open_issue': endpoint + f'/issues/{issue_id}',
     }
-    url_value = query_dict.get(key, default=None)
+    url_value = query_dict.get(lookup, None)
     return url_value
 
 
@@ -62,17 +60,47 @@ def confirm_sync(request):
         owner = settings.USER
         repo = 'IssueTrackerSandbox'
         # query_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
-        key = 'rate_limit'
-        query_url = get_query_url(key)
-        params = {
-            'state':'open',
-        }
-        headers = {'Authorization': f'token {token}'}
+        lookup = 'get_issue'
+        issue_id = '5'
+        query_url = get_query_url(lookup, issue_id=issue_id)
+        print(query_url)
+        headers = {
+            'Authorization': f'token {token}',
+            # 'Accept': 'application/vnd.github.v3+json',
+            }
+        data = {
+            # 'state':'open',
+            'title':'TEST Issue2',
+            'body': 'some text',
+            'milestone': 1,
+            'labels': ['bug'],
+            }
+        # payload = json.dumps(data)
+        # headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github.v3+json'}
+        # headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github.v3+json'}
         # r = requests.get(query_url, headers=headers, params=params)
-        r = requests.get(query_url, headers=headers)
-        pprint(r.json())
+        # r = requests.post(query_url, params=data, headers=headers)
+        # r = requests.get(query_url, headers=headers)
+        # pprint(r.json())
+
+        # g = Github(token)
+        # repo = g.get_repo("RHAM231-IssueTracker/IssueTrackerSandbox")
+        # # i = repo.create_issue(
+        # #     title="Test close Title",
+        # #     body="Text of the body.",
+        # #     # assignee="MartinHeinz",
+        # #     labels=[
+        # #         repo.get_label("good first issue")
+        # #     ]
+        # # )
+        # i = repo.get_issue(7)
+        # e = i.edit(
+        #     state="closed",
+        # )
+        # pprint(i)
 
     return render(request, 'sync/confirm_sync.html')
+
 
 
 def sync_success(request):
