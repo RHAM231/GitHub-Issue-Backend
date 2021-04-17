015bf0a66fe09fd0328180993b7850d83fd7c383
Rex Mitchell <nogardjmj@gmail.com>
2021-04-17 16:10:08 -0500

    Sewing everything together

--- app/repositories/urls.py
+++ app/repositories/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
 from . views import (
-    RepositoryListView
+    RepositoryListView, RepoContentsListView, 
+    FolderContentsListView, FileDetailView,
 )
 
 
 urlpatterns = [
     # path('confirm-sync', views.confirm_sync, name='confirm-sync'),
     # path('sync-success', views.sync_success, name='sync-success'),
-    path('Repositories', RepositoryListView.as_view(), name='project-list'),
+    path('Repositories/', RepositoryListView.as_view(), name='project-list'),
+    path('Repositories/<int:root_id>/', RepoContentsListView.as_view(), name='project-contents'),
+    path('Repositories/Folders/<int:folder_id>/', FolderContentsListView.as_view(), name='folder-contents'),
+    path('Repositories/Folders/Files/<int:file_id>/', FileDetailView.as_view(), name='file-contents'),
 ]
--- app/repositories/views.py
+++ app/repositories/views.py
@@ -2,16 +2,8 @@ from django.shortcuts import render
 from django.views.generic import (
     ListView, DeleteView, DetailView, UpdateView
 )
-from . models import Repository, RepoFolder, RepoFile
+from . models import Repository, RepoFolder, RepoFile, LineOfCode
 from django.db.models import Count
-
-
-# def project_list(request):
-#     context = {
-#         'issue_count': 123,
-#         'title': 'Projects',
-#     }
-#     return render(request, 'repositories/project_list.html', context)
 
 
 class RepositoryListView(ListView):
@@ -23,9 +15,48 @@ class RepositoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # context['issue_count'] = self.object.issue_repo.all().count()
-
+        context['title'] = 'Projects'
         return context
 
+
+class RepoContentsListView(ListView):
+    model = RepoFolder
+    template_name = 'repositories/project_contents.html'
+    context_object_name = 'repo_contents'
+
+    def get_queryset(self):
+        repo_id = self.kwargs['root_id']
+        repo = Repository.objects.get(id=repo_id)
+        root_folder_name = repo.name + '_root'
+        root_folder = RepoFolder.objects.get(name='repo_root', repository=repo_id, parent_folder=None)
+        self.root = root_folder
+        queryset = RepoFolder.objects.all().filter(parent_folder=root_folder.id).annotate(issue_count=Count('repofolder'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Projects'
+        context['files'] = RepoFile.objects.all().filter(parent_folder=self.root.id).annotate(issue_count=Count('repofile'))
+        return context
+
+
+class FolderContentsListView(ListView):
+    model = RepoFolder
+    template_name = 'repositories/folder_contents.html'
+    context_object_name = 'folder_contents'
+
+    def get_queryset(self):
+        folder_id = self.kwargs['folder_id']
+        self.folder = folder_id
+        queryset = RepoFolder.objects.all().filter(parent_folder=folder_id).annotate(issue_count=Count('repofolder'))
+        print(queryset)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Projects'
+        context['files'] = RepoFile.objects.all().filter(parent_folder=self.folder).annotate(issue_count=Count('repofile'))
+        return context
 
 
 # def file_contents(request):
@@ -36,6 +67,17 @@ class RepositoryListView(ListView):
 #     return render(request, 'base/file_contents.html', context)
 
 
-# class file_contents(DetailView):
-#     model = RepoFile
-#     template_name = 'repositories/file_contents.html'
+class FileDetailView(DetailView):
+    model = RepoFile
+    template_name = 'repositories/file_contents.html'
+    context_object_name = 'file'
+    pk_url_kwarg = 'file_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Files'
+        lines = LineOfCode.objects.all().filter(repofile=self.kwargs['file_id'])
+        context['lines'] = lines
+        context['line_count'] = lines.count()
+        context['sloc'] = LineOfCode.objects.all().filter(repofile=self.kwargs['file_id']).exclude(content='').count()
+        return context
