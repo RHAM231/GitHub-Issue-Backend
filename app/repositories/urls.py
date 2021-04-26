964060cd4339d345417c31bcc31614d19334b663
Rex Mitchell <nogardjmj@gmail.com>
2021-04-26 17:23:58 -0500

    Adding path to urls

--- app/repositories/urls.py
+++ app/repositories/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
-from . import views
+from django.urls import path, register_converter
+from . import converters, views
 from . views import (
     RepositoryListView, RepoContentsListView, 
     FolderContentsListView, FileDetailView,
 )
 
+register_converter(converters.CustomPathConverter, 'cpath')
 
 urlpatterns = [
     # path('confirm-sync', views.confirm_sync, name='confirm-sync'),
     # path('sync-success', views.sync_success, name='sync-success'),
     path('', RepositoryListView.as_view(), name='project-list'),
-    path('test/<int:repo_id>/', RepoContentsListView.as_view(), name='project-contents'),
-    path('Folders/<int:folder_id>/', FolderContentsListView.as_view(), name='folder-contents'),
-    path('Folders/Files/<int:file_id>/', FileDetailView.as_view(), name='file-contents'),
+    path('<int:repo_id>/', RepoContentsListView.as_view(), name='project-contents'),
+    path('<cpath:path>/<int:folder_id>/', FolderContentsListView.as_view(), name='folder-contents'),
+    path('<cpath:path>/<int:file_id>/', FileDetailView.as_view(), name='file-contents'),
 ]
