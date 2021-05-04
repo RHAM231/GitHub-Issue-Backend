7f512817ead33c8d5a024eea6f11c596824e10ae
Rex Mitchell <nogardjmj@gmail.com>
2021-05-04 09:23:10 -0500

    Added user pages. Commit before removing path converter.

--- app/GITB/urls.py
+++ app/GITB/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
 
     path('Issues/', include('issues.urls')),
     path('Repositories/', include('repositories.urls')),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     # path('users/', include('users.urls')),
--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from . models import Profile
+# from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
+
+# @login_required
+def profile(request):
+    # if request.method == 'POST':
+    #     u_form = UserUpdateForm(request.POST, instance=request.user)
+    #     p_form = ProfileUpdateForm(request.POST,
+    #                                request.FILES,
+    #                                instance=request.user.profile)
+    #     if u_form.is_valid() and p_form.is_valid():
+    #         u_form.save()
+    #         p_form.save()
+    #         messages.success(request, f'Your account has been updated!')
+    #         return redirect('profile')
+
+    # else:
+    #     u_form = UserUpdateForm(instance=request.user)
+    #     p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    if request.user.__class__.__name__ == "AnonymousUser":
+        profile = Profile.objects.get(name='Guest')
+    else:
+        profile = Profile.objects.get(user=request.user)
+
+    context = {
+        # 'u_form': u_form,
+        # 'p_form': p_form,
+        'title': 'Users',
+        'profile': profile
+    }
+
+    return render(request, 'users/profile.html', context)
