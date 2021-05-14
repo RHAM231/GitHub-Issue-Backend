88e7baa1320a4b65c63ebf11faf1e48736abfaa1
Rex Mitchell <nogardjmj@gmail.com>
2021-05-14 17:25:37 -0500

    Finished adding dynamic dropdowns for fields

--- app/issues/forms.py
+++ app/issues/forms.py
@@ -66,6 +66,7 @@ class IssueStateFilterForm(forms.Form):
     btn = forms.CharField()
 
 
+# Issue form for creating and updating issues
 class IssueEntryForm(ModelForm):
     class Meta:
         model = Issue
@@ -74,58 +75,50 @@ class IssueEntryForm(ModelForm):
             'associated_file', 'associated_loc'
             ]
 
+    # Override the form's init method to set labels, classes, and populate dropdown data
     def __init__(self, *args, **kwargs):
         super(IssueEntryForm, self).__init__(*args, **kwargs)
 
-        self.fields['associated_file'].queryset = RepoFile.objects.none()
-        self.fields['associated_loc'].queryset = LineOfCode.objects.none()
-
-
+        # Set custom labels on our association fields
         self.fields['associated_folder'].label = 'Folder'
         self.fields['associated_file'].label = 'File'
         self.fields['associated_loc'].label = 'Line of Code'
 
-
+        # Set a class for all our fields to make CSS cleaner
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'issue-form-field'
 
+        # Set a special class for our association fields so we can style them differently
         for key, field in self.fields.items():
             if key.startswith('associated_'):
                 field.widget.attrs = {'class': 'associate-field'}
 
-        print(self.data)
-
-        if 'associated_folder' in self.data:
-            print('trying ...')
-            try:
-                folder_id = int(self.data.get('associated_folder'))
-                print(folder_id)
-                self.fields['associated_file'].queryset = RepoFile.objects.filter(parent_folder=folder_id).order_by('name')
-                print(self.fields['associated_file'].queryset)
-            except (ValueError, TypeError):
-                print('passed')
-                pass
-        elif self.instance.pk:
-            print('printing from within elif')
-            self.fields['associated_file'].queryset = self.instance.parent_folder.folder_repo.order_by('name')
-            print(self.fields['associated_file'].queryset)
-        
-        if 'associated_file' in self.data:
-            print('trying ...')
-            try:
-                file_id = int(self.data.get('associated_file'))
-                print(file_id)
-                self.fields['associated_loc'].queryset = LineOfCode.objects.filter(repofile=file_id).order_by('line_number')
-                print(self.fields['associated_loc'].queryset)
-            except (ValueError, TypeError):
-                print('passed')
-                pass
-        elif self.instance.pk:
-            print('printing from within elif')
-            self.fields['associated_loc'].queryset = self.instance.repofile.loc.order_by('line_number')
-            print(self.fields['associated_loc'].queryset)
+        # If the form already has data when loaded (update/edit) set the querysets
+        # for the file and line of code fields based on the ids of the folder and
+        # file fields
+        # Check for existing data
+        if kwargs['instance']:
+            # Check for folder data
+            if kwargs['instance'].associated_folder:
+                # Get our folder id
+                folder_id = kwargs['instance'].associated_folder.id
+                # Set the file queryset for the dropdown
+                self.fields['associated_file'].queryset = RepoFile.objects.filter(
+                        parent_folder=folder_id).order_by('name')
+            # Otherwise start the value as empty
+            else:
+                self.fields['associated_file'].queryset = RepoFile.objects.none()
+            
+            # Repeat for lines of code
+            if kwargs['instance'].associated_file:
+                file_id = kwargs['instance'].associated_file.id
+                self.fields['associated_loc'].queryset = LineOfCode.objects.filter(
+                        repofile=file_id).order_by('line_number')
+            else:
+                self.fields['associated_loc'].queryset = LineOfCode.objects.none()
 
 
+# Form for changing the state of an issue. Used on the issue detail page
 class OpenCloseIssueForm(ModelForm):
     class Meta:
         model = Issue
