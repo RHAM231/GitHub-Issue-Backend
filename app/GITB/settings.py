c8d40015c9c7732a9e311870cddcfad9a447fc65
Rex Mitchell <nogardjmj@gmail.com>
2021-05-19 17:08:41 -0500

    Updating settings.py

--- app/GITB/settings.py
+++ app/GITB/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rz0s^qc^&k06=3ftch)7^iwuu&922d-odux-qx8ik=h%+5tr%e'
+SECRET_KEY = os.environ.get('IT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
