2a89a8dc31aade0763d0c654258f78e6df6705fb
Rex Mitchell <nogardjmj@gmail.com>
2021-05-27 17:59:51 -0500

    Added expire_events.py to enable cron to periodically delete sandbox-import repo

--- app/sync/management/commands/expire_events.py
+++ app/sync/management/commands/expire_events.py
@@ -1,10 +1,32 @@
 from repositories.models import Repository
-from django.utils import timesince, timezone
+from django.utils import timezone
+from datetime import timedelta
 from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
 
 
+# Let's add a command to manage.py to allow a cron job on our server
+# to periodically delete our Sandbox-Import repo. We'll have cron run
+# the command every 15 minutes to check if the repo was created over
+# an hour ago.
 class Command(BaseCommand):
     help = 'Expires event objects which are out-of-date'
     def handle(self, *args, **kwargs):
-        print('test')
-        Repository.objects.filter(name='Sandbox-Import').delete()
+        # Try to get our repo
+        try:
+            repo = Repository.objects.get(name='Sandbox-Import')
+            # Get the length of the time the repo has existed in the database
+            time_check = timezone.now() - repo.expire_time
+            # Set our expiry time to one hour
+            expire = timedelta(hours=1)
+
+            # If an hour has passed, delete the repo, otherwise do nothing
+            if time_check >= expire:
+                print('Repository expired, deleting ...')
+                repo.delete()
+            else:
+                print('Repository not expired yet, keeping ...')
+
+        except ObjectDoesNotExist:
+            print('No repository to delete')
+        
